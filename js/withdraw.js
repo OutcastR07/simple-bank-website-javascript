eb66bf63fdeca09841de3ce46504edec8a187812
OutcastR97 <sadman.rashiq.dbz@gmail.com>
2022-08-20 21:41:05 +0600

    small adjustment

--- js/withdraw.js
+++ js/withdraw.js
@@ -3,17 +3,31 @@ document.getElementById('btn-withdraw').addEventListener('click', function () {
     const newWithdrawAmountString = withdrawField.value;
     const newWithdrawAmount = parseFloat(newWithdrawAmountString);
 
+    if (isNaN(newWithdrawAmount)) {
+        alert('Please insert amount');
+        withdrawField.value = '';
+        return;
+    }
+
     const withdrawTotalElement = document.getElementById('withdraw-total');
     const previousWithdrawTotalString = withdrawTotalElement.innerText;
     const previousWithdrawTotal = parseFloat(previousWithdrawTotalString);
 
-    const currentWithdrawTotal = newWithdrawAmount + previousWithdrawTotal;
 
-    withdrawTotalElement.innerText = currentWithdrawTotal;
 
     const balanceTotalElement = document.getElementById('balance-total');
     const previousBalanceTotalString = balanceTotalElement.innerText;
     const previousBalanceTotal = parseFloat(previousBalanceTotalString);
+
+    if (newWithdrawAmount > previousBalanceTotal) {
+        alert('Limit exceeded!');
+        withdrawField.value = '';
+        return;
+    }
+
+    const currentWithdrawTotal = newWithdrawAmount + previousWithdrawTotal;
+
+    withdrawTotalElement.innerText = currentWithdrawTotal;
 
     currentBalanceTotal = previousBalanceTotal - newWithdrawAmount;
 
